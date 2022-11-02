2696c617b6e67f4eceeff1b9f5975a5bc315f0ca
Jim <jimba.ed@outlook.com>
2022-11-02 11:29:05 -0400

    commit tree 9fd832adec7d1b8eb7e41ca7ca00e9e0e6cdcb3e

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,29 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: {in: ['Fiction', 'Non-Fiction']}
+    validate :title_is_clickbate
+
+    
+
+    def title_is_clickbate
+
+        if title == nil
+            return
+        end
+
+    clickbateTitle = [
+            "Won't Believe",
+            "Secret",
+            "Top [number]",
+            "Guess"
+        ]
+
+        if clickbateTitle.none? {|c| title.include?(c)}
+            errors.add(:title, "Not clickbait-y enough!")
+        # elsif title == nil
+        #     return
+        end
+    end
 end
